6d8b064b9f0f61c1aa45311bb2f72d8c85b31620
Helder-Lucau <helderlucau@gmail.com>
2023-10-10 10:22:34 +0300

    completed

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,12 @@
 class Course:
-    pass
+  def __init__(self, title='title', schedule='schedule', description='description'):
+    self.title = title
+    self.schedule = schedule
+    self.description = description
+
+  def __str__(self):
+    output = ''
+    output += f'Title: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n'
+    output += '------------------'
+    return output
+
